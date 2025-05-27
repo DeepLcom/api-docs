7690020c40b8ccb4b981b462fdbe918e04a9db90
Leon Cheng <leon.cheng@deepl.com>
2025-05-27 15:42:12 -0400

    feat: Add some basic analytics
    - point to dev environment for now
    - point to web table for now
    - write TODOs for future field updates

---
+++ analytics.js
@@ -0,0 +1,41 @@
+const STATISTICS_URL = "https://s.deepl.dev/web/statistics"
+
+const sendToDAP = async (getPayload) => {
+  try {
+    const response = await fetch(
+      STATISTICS_URL, 
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(getPayload()),
+        method: "POST",
+      })
+    const data = await response.json();
+    console.log('Fetched data: ', data); // TODO remove
+    return data;
+  } catch (error) {
+    console.error('Fetch error: ', error);
+  }
+}
+
+const onPageLoad = () => {
+  sendToDAP(() => ({
+    instanceId: "1111111111-1111-1111-1111-111111111111", // TODO figure out how to get the instance_id
+    sessionId: "00000000-0000-0000-0000-000000000000", // TODO remove when new table is created
+    eventId: 290, // TODO update 
+    pageId: 110, // TODO update
+    url: window.location.href,
+    userAgent: navigator.userAgent,
+    interfaceLanguage: navigator.language,
+    screenInfo: {
+        widthCssPixel: window.screen.width,
+        heightCssPixel: window.screen.height,
+        viewportWidthCssPixel: window.innerWidth,
+        viewportHeightCssPixel: window.innerHeight,
+        devicePixelRatio: window.devicePixelRatio || 1
+    }
+  }))
+}
+
+onPageLoad()
