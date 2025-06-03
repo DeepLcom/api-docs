5d6b8b6720af17aba50710c0b73e688eff63a029
Leon Cheng <leon.cheng@deepl.com>
2025-06-03 16:14:15 -0400

    feat: Implement sid and uid for analytics with deepl cookie

--- analytics.js
+++ analytics.js
@@ -1,6 +1,147 @@
+/*
+========================
+  Globals
+========================
+*/
 const STATISTICS_URL = "https://s.deepl.dev/web/statistics"
 
+/*
+========================
+  Cookie helper methods
+========================
+*/
+const getCookieItem = (cookieName) => {
+  if (typeof window === 'undefined') {
+    throw new Error('Error occurs when you are going to use a browser-specific code on server-side or other way around. Please check your code logic.')
+  }
+
+  const cookies = document.cookie.split('; ') || []
+  const cookie = cookies.find(c => c.startsWith(`${cookieName}=`)) || ''
+  const value = cookie.split('=').at(1)
+
+  return (
+    value
+      ? decodeURIComponent(value)
+      : value
+  )
+}
+
+const setCookieItem = (cookieName, cookieValue, expiresInMinutes = 0, domain = '.deepl.com') => {
+  if (typeof window === 'undefined') {
+    throw new Error('Error occurs when you are going to use a browser-specific code on server-side or other way around. Please check your code logic.')
+  }
+
+  const cookieValueToStore = (
+    typeof cookieValue === 'string' || typeof cookieValue === 'number'
+      ? cookieValue
+      : JSON.stringify(cookieValue)
+  )
+
+  let expireString = ''
+  if (expiresInMinutes) {
+    const date = new Date()
+    const expiresInMilliseconds = expiresInMinutes * 60 * 1000 // SECONDS * MILLISECONDS
+
+    date.setTime(date.getTime() + expiresInMilliseconds)
+    expireString = `; expires=${date.toUTCString()}`
+  }
+
+  let domainString = ''
+  if (domain) {
+    domainString = `; domain=${domain}`
+  }
+
+  const secure = '; secure'
+  document.cookie = `${cookieName}=${encodeURIComponent(cookieValueToStore)}${domainString}; path=/${expireString}; samesite=lax${secure}`
+}
+
+const mkUUID = () => {
+  if (typeof window === 'undefined') {
+    return ''
+  }
+
+  if (!Object.prototype.hasOwnProperty.call(window, 'crypto')) {
+    return ''
+  }
+
+  if (typeof window.crypto.randomUUID === 'function') {
+    return window.crypto.randomUUID()
+  }
+
+  return [1e7, -1e3, -4e3, -8e3, -1e11].join('').replace(/[018]/g, c =>
+    (parseInt(c, 10) ^ (window.crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c, 10) / 4)))).toString(16))
+}
+
+const DAP_UID_KEY = 'dapUid'
+const DAP_SID_KEY = 'dapSid'
+const YEAR_IN_MINUTES = 60 * 24 * 30 * 12
+const SID_TTL_MINUTES = 30;
+
+const getUid = () => {
+  let uid = getCookieItem(DAP_UID_KEY)
+  if (uid === undefined) {
+    setCookieItem(
+      DAP_UID_KEY,
+      mkUUID(),
+      YEAR_IN_MINUTES
+    )
+  }
+  uid = getCookieItem(DAP_UID_KEY)
+  return uid
+}
+
+
+const getSid = () => {
+  let sidCookie = getCookieItem(DAP_SID_KEY);
+
+  let sidObj;
+  if (sidCookie) {
+    try {
+      sidObj = JSON.parse(sidCookie);
+    } catch {
+      sidObj = null;
+    }
+  }
+
+  const now = Date.now();
+
+  if (sidObj && sidObj.sid && sidObj.lastUpdate) {
+    // SID exists already
+    // Check if >30 minutes old
+    if (now - sidObj.lastUpdate > SID_TTL_MINUTES * 60 * 1000) {
+      // Refresh SID
+      sidObj = {
+        sid: mkUUID(),
+        lastUpdate: now
+      };
+    } else {
+      // Keep SID, reset TTL
+      sidObj = {
+        ...sidObj,
+        lastUpdate: now
+      };
+    }
+  } else {
+    // No SID, create new
+    sidObj = {
+      sid: mkUUID(),
+      lastUpdate: now
+    };
+  }
+
+  setCookieItem(DAP_SID_KEY, JSON.stringify(sidObj), SID_TTL_MINUTES);
+  return sidObj.sid;
+}
+
+/*
+========================
+  Sending Requests
+========================
+*/
 const baseRequest = async (targetUrl, getPayload) => {
+  const payload = getPayload()
+  console.log(payload)
+
   try {
     const response = await fetch(
       targetUrl, 
@@ -8,7 +149,7 @@ const baseRequest = async (targetUrl, getPayload) => {
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify(getPayload()),
+        body: JSON.stringify(payload),
         method: "POST",
       })
     const data = await response.json();
@@ -25,8 +166,8 @@ const requestToDAP = (eventType, extraFields) => {
   baseRequest(
     STATISTICS_URL,
     () => ({
-      instanceId: "11111111-1111-1111-1111-111111111111", // TODO figure out how to get the instance_id
-      sessionId: "00000000-0000-0000-0000-000000000000", // TODO remove when new table is created
+      instanceId: getUid(),
+      sessionId: getSid(),
       eventId: 290, // TODO update 
       pageId: 110, // TODO update
       userAgent: navigator.userAgent,
@@ -139,6 +280,7 @@ const setupNetworkRequestTracking = () => {
 const onPageLoad = () => {
   setupNavigationTrackers();
   setupNetworkRequestTracking();
+
   sendPageviewDeduped('navigation:newPageLoad')
 }
 onPageLoad()
