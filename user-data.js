d26e16cca83ead9ab670364202e80bced36a8b62
Ben Morss <morss@google.com>
2025-10-15 12:33:46 -0400

    Testing out the ability to call w.deepl.com

---
+++ user-data.js
@@ -0,0 +1,32 @@
+const h1 = document.getElementById('page-title');
+const apiBaseUrl = 'https://w-test.deepl.com/web'; //TODO: change this when we're in prod
+const apiParams = 'request_type=jsonrpc&il=en&method=getClientState';
+
+const apiUrl = `${apiBaseUrl}/${apiParams}`;
+
+(async () => {
+  const hasApiSubscription = await getApiSubscription();
+
+  if (hasApiSubscription) {
+    const cta = document.getElementById('topbar-cta-button');
+    const ctaSpans = Array.from(cta.querySelectorAll('span'));
+    const ctaButtonSpan = ctaSpans.find(el => el.textContent === 'Create free API account');
+    ctaButtonSpan.innerHTML = 'Manage API keys';
+    // can also change colors
+  }
+})();
+
+
+//TODO: error-handling. Like what if the user isn't logged in, or there is no user?
+
+async function getApiSubscription() {
+  try {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    
+    return json?.result?.featureSet?.subscription?.api;
+
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+  }
+}
