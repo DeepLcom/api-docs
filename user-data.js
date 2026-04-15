031e34196dc607c93985bf41e355687c534b3556
Shir Goldberg <3937986+shirgoldbird@users.noreply.github.com>
2026-04-15 17:08:40 -0400

    Simplify account id lookup for client state

--- user-data.js
+++ user-data.js
@@ -5,34 +5,6 @@ window.DeepLUser = (() => {
   const API_BASE_URL = 'https://w.deepl.com/web';
   const API_PARAMS = 'request_type=jsonrpc&il=en&method=getClientState';
   const apiUrl = `${API_BASE_URL}?${API_PARAMS}`;
-  const ACCOUNT_ID_KEYS = ['accountId', 'accountID', 'account_id'];
-
-  function findValueByKeys(value, keys) {
-    if (!value || typeof value !== 'object') return undefined;
-
-    for (const key of keys) {
-      if (value[key] !== undefined && value[key] !== null) {
-        return value[key];
-      }
-    }
-
-    for (const nestedValue of Object.values(value)) {
-      const match = findValueByKeys(nestedValue, keys);
-      if (match !== undefined) return match;
-    }
-
-    return undefined;
-  }
-
-  function extractAccountId(clientState) {
-    if (!clientState) return undefined;
-
-    return (
-      findValueByKeys(clientState, ACCOUNT_ID_KEYS)
-      || clientState.account?.id
-      || clientState.subscription?.account?.id
-    );
-  }
 
   /* Following the standards of deepl.com, we place the request parameters in both the URL and the body,
    * although this may not be necessary.
@@ -72,8 +44,12 @@ window.DeepLUser = (() => {
 
   async function getAccountId() {
     const clientState = await getClientState();
-    return extractAccountId(clientState);
+    return clientState?.accountId;
   }
 
   return { getClientState, getApiSubscription, getAccountId };
 })();
+
+window.getDeepLClientStateNow = function getDeepLClientStateNow() {
+  return window.DeepLUser?.getClientState?.();
+};
